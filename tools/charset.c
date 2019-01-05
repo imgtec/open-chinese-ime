1d7ccfd292c749147b260171ea19246134303714
duhuanpeng <u74147@gmail.com>
2019-01-05 10:15:20 +0800

    字符集工具：生成国标一级汉字
    Signed-off-by: duhuanpeng <u74147@gmail.com>

---
+++ tools/charset.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+	int col, row;
+	int count = 0;
+
+	/* 先打印 GB2312 再说 */
+	
+	for(row = 16; row <= 55; row++){
+		/* 一级汉字 3755 个 */
+		for(col = 1; col <= 94; col++) {
+			int r, c;
+			r = row + 0xa0;
+			c = col + 0xa0;
+
+			count++;
+#if 1
+			/* EXCEL 格式 */
+			printf("%d,%c%c\n", count, r, c);
+#else
+			putchar(r);
+			putchar(c);
+#endif
+			if(count >= 3755) break;
+		}
+		putchar('\n');
+	}
+
+	/* 二级汉字 3008 个 */
+
+	printf("%d\n", count);
+
+	return 0;
+}
+
